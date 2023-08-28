72511ccf9b0a2813b868a7a3aba39dfeb71caa8e
Muskaan <muskaanshakilshaikh721@gmail.com>
2023-08-28 12:02:48 +0530

    Fetch history-postgres

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const { Pool } = require('pg'); // Import the pg library
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'currency_conversion',
+    password: '@07032001m.',
+    port: 5432,
+});
+
 app.use(express.static('public'));
+
+app.use(express.json()); // Add this line to parse JSON requests
 
 app.get('/convert', (req, res) => {
     const sourceCurrency = req.query.sourceCurrency;
     const targetCurrency = req.query.targetCurrency;
     const amount = parseFloat(req.query.amount);
-
-    // Mock exchange rates (you can replace with actual rates)
     const exchangeRates = {
         INR: {
             USD: 0.014,
@@ -26,3 +36,33 @@ app.get('/convert', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+app.post('/logConversion', async (req, res) => {
+    const { sourceCurrency, sourceAmount, targetCurrency, convertedAmount } = req.body;
+    
+    const query = `
+        INSERT INTO conversion_logs (source_currency, source_amount, target_currency, converted_amount)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id;
+    `;
+
+    try {
+        const result = await pool.query(query, [sourceCurrency, sourceAmount, targetCurrency, convertedAmount]);
+        res.status(201).send({ id: result.rows[0].id });
+    } catch (error) {
+        console.error('Error logging conversion:', error);
+        res.status(500).send('Internal server error');
+    }
+});
+
+app.get('/getHistory', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM conversion_logs ORDER BY date DESC');
+        res.send(result.rows);
+    } catch (error) {
+        console.error('Error fetching history:', error);
+        res.status(500).send('Internal server error');
+    }
+});
+
--- public/script.js
+++ public/script.js
@@ -7,6 +7,55 @@ document.getElementById('converter-form').addEventListener('submit', async (even
 
     const response = await fetch(`/convert?sourceCurrency=${sourceCurrency}&targetCurrency=${targetCurrency}&amount=${amount}`);
     const data = await response.json();
-    console.log(typeof(response));
+    
     document.getElementById('result').innerText = `Converted Amount: ${data.convertedAmount.toFixed(2)} ${targetCurrency}`;
+        
+        const logData = {
+            sourceCurrency,
+            sourceAmount: amount,
+            targetCurrency,
+            convertedAmount: data.convertedAmount,
+        };
+    
+        const logResponse = await fetch('/logConversion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(logData),
+        });
+    
+        if (logResponse.status === 201) {
+            console.log('Conversion data logged successfully');
+        } else {
+            console.error('Failed to log conversion data');
+        }
 });
+
+document.getElementById('fetch-history-button').addEventListener('click', async () => {
+    const response = await fetch('/getHistory');
+    const history = await response.json();
+
+    const historyTable = document.getElementById('history-table');
+    historyTable.innerHTML = `
+        <tr>
+            <th>Date</th>
+            <th>Source Currency</th>
+            <th>Source Amount</th>
+            <th>Target Currency</th>
+            <th>Converted Amount</th>
+        </tr>
+    `;
+
+    history.forEach(log => {
+        const row = historyTable.insertRow();
+        row.insertCell().innerText = log.date;
+        row.insertCell().innerText = log.source_currency;
+        row.insertCell().innerText = log.source_amount;
+        row.insertCell().innerText = log.target_currency;
+        row.insertCell().innerText = log.converted_amount;
+    });
+});
+
+
+
